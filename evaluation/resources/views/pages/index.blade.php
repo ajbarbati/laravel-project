320fcd542c2477b434c5dc4b3fb819104ad04c1b
Alex <ajbarbati@gmail.com>
2020-11-06 17:26:19 -0500

    implemented bootstrap the old fashioned way

--- evaluation/resources/views/pages/index.blade.php
+++ evaluation/resources/views/pages/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Welcom to Laravel</h1>
+<h1>Welcome to my Notes App</h1>
+<h2>How to use!</h2>
+<ol>
+    <li>View current Notes</li>
+    <li>Click on a Note to View and Edit it</li>
+    <li>Create a New Note</li>
+</ol>
 @endsection
